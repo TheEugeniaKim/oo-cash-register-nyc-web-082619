9c2ba779ef8c296968aacbb546a38c0337ed5222
Eugenia Kim <elk303@nyu.edu>
2019-08-28 13:06:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
-
+require "pry"
+class CashRegister 
+  attr_accessor :total, :discount, :items 
+  
+  def initialize(discount=0)
+    @total = 0 
+    self.discount = discount
+    @items = []
+  end
+  
+  def add_item(title, price, quantity=1)
+    @total = @total + (price*quantity) 
+    quantity.items.push(title)
+  end
+  
+  5.times do
+    puts
+  
+  def apply_discount
+    if @discount>0
+    discount_amount = (@discount/100.00)*@total 
+    @total = @total - discount_amount
+    return "After the discount, the total comes to $#{total.to_i}."
+    else 
+      return "There is no discount to apply."
+    end
+  end
+  
+end 
