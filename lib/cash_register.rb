18516ccf3499133c40516fb6089f6974afa2a180
Eugenia Kim <elk303@nyu.edu>
2019-08-28 13:30:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,7 @@ class CashRegister
   def add_item(title, price, quantity=1)
     @total = @total + (price*quantity)
     binding.pry
+    quantity.times do 
     @items.push(title)
     # @items = quantity.times do @items.push(title)
     
